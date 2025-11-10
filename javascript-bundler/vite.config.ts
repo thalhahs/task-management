3aa9cc6931938ef52731e660970869c868e602cd
thalhahs <thalhah@gmail.com>
2025-11-10 19:40:42 +0700

    chore: fresh start

--- javascript-bundler/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  root: ".",
-  server: {
-    port: 5173
-  }
-});
--- javascript-modules/index.js
+++
@@ -1,3 +0,0 @@
-import { add } from "./lib.js";
-
-console.log(add(1, 2));
