e240fb8a6ff3e9d133b3c4a6e3aabe2e0a814d13
thalhahs <thalhah@gmail.com>
2025-11-17 21:27:36 +0700

    feat: handle increment

--- src/modules/counter/counter-increment.tsx
+++ src/modules/counter/counter-increment.tsx
@@ -1,11 +1,17 @@
+import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
 export function CounterIncrement() {
   const [count, setCount] = useState(0);
 
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
   return (
     <div>
       <p>Count Value: {count}</p>
+      <Button onClick={handleIncrement}>Increment</Button>
     </div>
   );
 }
