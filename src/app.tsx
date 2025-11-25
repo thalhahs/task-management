c553e6eac1a7d397349cb892025ef7da91a6935a
thalhahs <thalhah@gmail.com>
2025-11-25 21:31:48 +0700

    feat: navbar in layout route

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,9 @@
-import { Link } from "react-router";
 import { Tasks } from "./modules/task/tasks";
 
 export function App() {
   return (
     <div className="flex justify-center">
       <main className="w-full max-w-lg space-y-8">
-        <nav className="flex items-center justify-between gap-4">
-          <h1 className="my-4 text-2xl font-bold text-sky-700">
-            <Link to="/">Task Management</Link>
-          </h1>
-
-          <ul className="inline-flex gap-4">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Tasks />
       </main>
     </div>
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router";
+import { Link, Outlet } from "react-router";
 
 export function Layout() {
   return (
     <div>
-      <h1>Layout</h1>
+      <nav className="flex items-center justify-between gap-4">
+        <h1 className="my-4 text-2xl font-bold text-sky-700">
+          <Link to="/">Task Management</Link>
+        </h1>
+
+        <ul className="inline-flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
 
       <Outlet />
     </div>
