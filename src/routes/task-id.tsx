70fa8db98ddbfce4aa15dd100d3790022c487ece
thalhahs <thalhah@gmail.com>
2025-12-01 11:18:56 +0700

    feat: task id from params

--- src/routes/task-id.tsx
+++ src/routes/task-id.tsx
@@ -1,7 +1,12 @@
+import { useParams } from "react-router";
+
 export function TaskId() {
+  const params = useParams();
+  const { taskId } = params;
+
   return (
     <div>
-      <h1>Task ID</h1>
+      <h1>Task ID: {taskId}</h1>
     </div>
   );
 }
